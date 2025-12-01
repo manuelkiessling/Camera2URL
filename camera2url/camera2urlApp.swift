297ca16c1fd3dfc2c95b355486bffa5d38e31720
Manuel Kießling <manuel@kiessling.net>
2025-12-01 15:40:10 +0100

    Initial Commit

---
+++ camera2url/camera2urlApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera2urlApp.swift
+//  camera2url
+//
+//  Created by Manuel Kießling on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct camera2urlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ camera2urlTests/camera2urlTests.swift
@@ -0,0 +1,17 @@
+//
+//  camera2urlTests.swift
+//  camera2urlTests
+//
+//  Created by Manuel Kießling on 01.12.25.
+//
+
+import Testing
+@testable import camera2url
+
+struct camera2urlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
